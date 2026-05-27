c81cbda16c6361f145705cb998eca8e9cba8faac
XeKu <sb9462008@gmail.com>
2026-05-27 15:48:00 +0800

    Fix App.jsx: add Provider, store, landing page with company name and Get Started button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
 import ProductList from './components/ProductList';
 import CartItem from './components/CartItem';
 import './App.css';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -27,35 +36,30 @@ function App() {
     setShowCart(false);
   };
 
-  if (showLanding) {
-    return (
-      <div className="landing-page">
-        <div className="landing-content">
-          <h1>Paradise Nursery</h1>
-          <p>Where Green Meets Serenity</p>
-          <button className="get-started-btn" onClick={handleGetStarted}>
-            Get Started
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  if (showCart) {
-    return (
-      <CartItem
-        onContinueShopping={handleShowProducts}
-        onHome={handleHome}
-        onCartClick={handleShowCart}
-      />
-    );
-  }
-
   return (
-    <ProductList
-      onCartClick={handleShowCart}
-      onHome={handleHome}
-    />
+    <Provider store={store}>
+      {showLanding && (
+        <div className="landing-page">
+          <div className="landing-content">
+            <h1>Paradise Nursery</h1>
+            <p>Where Green Meets Serenity</p>
+            <button className="get-started-btn" onClick={handleGetStarted}>
+              Get Started
+            </button>
+          </div>
+        </div>
+      )}
+      {!showLanding && !showCart && (
+        <ProductList onCartClick={handleShowCart} onHome={handleHome} />
+      )}
+      {!showLanding && showCart && (
+        <CartItem
+          onContinueShopping={handleShowProducts}
+          onHome={handleHome}
+          onCartClick={handleShowCart}
+        />
+      )}
+    </Provider>
   );
 }
 
